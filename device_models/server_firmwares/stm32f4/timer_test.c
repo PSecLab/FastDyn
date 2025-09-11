7053eb47bc2d9db8853719a4c53f073fab5aff0b
Muhammad Hammad Bashir <hammad.bashir543@gmail.com>
2025-09-11 01:34:52 -0400

    Configure PortG13 Led for the stm32f429 board

--- device_models/server_firmwares/stm32f4/timer_test.c
+++ device_models/server_firmwares/stm32f4/timer_test.c
@@ -27,10 +27,12 @@
 #define RCC_AHB1ENR         (*(volatile uint32_t*)(RCC_BASE + 0x30))
 #define RCC_APB2ENR         (*(volatile uint32_t*)(RCC_BASE + 0x44))
 
-// GPIOD
-#define GPIOD_BASE          0x40020C00
-#define GPIOD_MODER         (*(volatile uint32_t*)(GPIOD_BASE + 0x00))
-#define GPIOD_ODR           (*(volatile uint32_t*)(GPIOD_BASE + 0x14))
+// GPIOG registers
+#define PERIPH_BASE     0x40000000UL
+#define AHB1PERIPH_BASE (PERIPH_BASE + 0x00020000UL)
+#define GPIOG_BASE      (AHB1PERIPH_BASE + 0x1800UL)
+#define GPIOG_MODER     (*(volatile unsigned int *)(GPIOG_BASE + 0x00))
+#define GPIOG_ODR       (*(volatile unsigned int *)(GPIOG_BASE + 0x14))
 
 // TIM1 (Advanced-control timer)
 #define TIM1_BASE           0x40010000
@@ -48,6 +50,8 @@
 // Define IRQ numbers for TIM1
 #define TIM1_UP_TIM10_IRQn  25
 
+#define LED_GREEN  13
+
 /**
  * @brief  This is the interrupt service routine for TIM1 Update and TIM10 global interrupts.
  * It gets called when the TIM1 counter overflows.
@@ -55,8 +59,8 @@
 void TIM1_UP_TIM10_IRQHandler(void) {
     // Check if the update interrupt flag is set
     if (TIM1_SR & (1 << 0)) {
-        // Toggle the green LED (PD12)
-        GPIOD_ODR ^= (1 << 12);
+        // Toggle the green LED (PG13)
+        GPIOG_ODR ^= (1 << LED_GREEN);
 
         // CRITICAL: Clear the update interrupt flag.
         // If you don't do this, the ISR will be called again immediately after exiting.
@@ -68,12 +72,12 @@ void TIM1_UP_TIM10_IRQHandler(void) {
  * @brief  Main program.
  */
 int timer_test(void) {
-    // 1. Enable the clock for GPIOD
-    RCC_AHB1ENR |= (1 << 3); // Set bit 3 to enable GPIOD clock
+    // 1. Enable clock for GPIOG
+    RCC_AHB1ENR |= (1 << 6); // GPIOG enable
 
-    // 2. Configure PD12 (Green LED) as a general-purpose output
-    GPIOD_MODER &= ~(0b11 << 24); // Clear bits 24 and 25
-    GPIOD_MODER |= (0b01 << 24);  // Set bits to 01 for General purpose output mode
+    // 2. Configure PG13 (Green LED) as a general-purpose output
+    GPIOG_MODER &= ~((0x3 << (LED_GREEN*2)));
+    GPIOG_MODER |=  (0x1 << (LED_GREEN*2));
 
     // 3. Enable the clock for TIM1
     RCC_APB2ENR |= (1 << 0); // Set bit 0 to enable TIM1 clock
