4b9edc1da19f7e2a37486dec105ad2797253dab6
MuhammadHammad001 <hammad.bashir543@gmail.com>
2025-10-01 17:02:30 -0400

    Qemu started

--- src/fastdyn/main.py
+++ src/fastdyn/main.py
@@ -4,6 +4,7 @@ Main file is responsible for kicking the qemu command.
 import logging
 import argparse
 import os, shutil
+import subprocess
 
 from dotenv import load_dotenv
 
@@ -18,11 +19,58 @@ fastdyn_log.setLogConfig()
 
 
 #build qemu command
-def build_qemu_cmd(dev_config, dev_config_path):
+def build_qemu_cmd(config, dev_config_path):
+    """Builds the full qemu-system-arm command from the configuration."""
+    #----------------------------------------QEMU & CPU configurations---------------------------------------
+    cpu = config.dev_config.cpu  #short-hand
 
-    cmd = {
+    cmd = [cpu.get('qemu_path')]
 
-    }
+    cpu_configs = [
+        "-machine", f"{cpu['machine']},memory-backend=ram0",
+        "-cpu", cpu['cpu'],
+        "-kernel", cpu['binary'],
+        "-qmp", f"unix:{cpu['qmp_socket']},server,nowait",
+        "-d", cpu['log_options'],
+        "-D", cpu['log_file'],
+        "-monitor", f"tcp:127.0.0.1:{cpu['monitor_port']},server,nowait"
+    ]
+
+    if cpu.get('enable_gdb'):
+        log.info("GDB debugging enabled on Port 1234")
+        cpu_configs.append('-s')
+    if cpu.get('stop_on_start'): cpu_configs.append('-S')
+    if cpu.get('semihosting'):
+        cpu_configs.extend([
+            "--semihosting",
+            "--semihosting-config",cpu.get('semihosting_config', "enable=on,target=native")
+        ])
+    cmd.extend(cpu_configs)
+
+    #----------------------------------------Memory Configurations------------------------------------------
+    memory = config.dev_config.memory   #short-hand
+    ram0_path = os.path.join(memory.get('shared_mem_path', '/dev/shm'), memory.get('main_ram_file'))
+    ram1_path = os.path.join(memory.get('shared_mem_path', '/dev/shm'), memory.get('shared_ram_file'))
+
+    memory_configs = [
+        '-object', f"memory-backend-file,id=ram0,mem-path={ram0_path},size={memory.get('main_ram_size')},share=on",
+        '-object', f"memory-backend-file,id=ram1,mem-path={ram1_path},size={memory.get('shared_ram_size')},share=on",
+        '-global', 'cortexm-soc.shram_backend=ram1',
+        '-global', f'cortexm-soc.ram_baseaddr={memory.get("ram_base_addr")}',
+        '-global', f'cortexm-soc.shram_baseaddr={memory.get("shared_ram_base_addr")}',
+        '-global', f'armv7m.init-nsvtor={memory.get("init_nsvtor")}',
+    ]
+
+    cmd.extend(memory_configs)
+
+    #----------------------------------------Plugins Configurations------------------------------------------
+    plugin_lib_path = cpu.get('plugin_library', './build/libfastdyn.so')
+
+    plugin_configs = [
+        '-plugin', f"{plugin_lib_path},dev={dev_config_path}",
+    ]
+
+    cmd.extend(plugin_configs)
 
     return cmd
 
@@ -41,7 +89,7 @@ def run_qemu(config, out_path):
         out_path = "tmp"
 
     if os.path.exists(out_path):
-        log.info(f"The output directory already exists at Path {out_path}. Deleting it!")
+        log.info(f"The output directory already exists at Path {os.path.abspath(out_path)}. Deleting it!")
         shutil.rmtree(out_path)
 
     log.info(f"Creating output directory at path: {os.path.abspath(out_path)}")
@@ -51,8 +99,51 @@ def run_qemu(config, out_path):
     dev_config_path = gen_config._gen_dev_config(config, out_path)
     log.info(f"Custom Devices Configuration written to : {dev_config_path}")
 
-    build_qemu_cmd(config, dev_config_path)
+    cmd = build_qemu_cmd(config, dev_config_path)
 
+    _start_execution(cmd)
+
+def _start_execution(qemu_cmd):
+    """
+    Starts the actual execution of qemu,
+    peripheral server with handlers to enable clean
+    exiting
+    """
+    log.info("Running the following QEMU command:")
+    print(" ".join(qemu_cmd))
+    kill_qemu_process()
+    qemu_proc = subprocess.Popen(qemu_cmd)
+    log.info("Letting QEMU Run")
+
+    try:
+        qemu_proc.wait()
+    except KeyboardInterrupt:
+        kill_qemu_process()
+
+def kill_qemu_process():
+    PORT = 5555
+
+    def get_pids_using_port(port):
+        try:
+            result = subprocess.check_output(["lsof", "-ti", f"tcp:{port}"])
+            pids = result.decode().strip().split('\n')
+            return [int(pid) for pid in pids if pid.strip()]
+        except subprocess.CalledProcessError:
+            return []
+
+    def kill_pids(pids):
+        for pid in pids:
+            try:
+                os.kill(pid, signal.SIGKILL)
+                subprocess.run(["stty", "sane"])
+            except ProcessLookupError:
+                print(f"PID {pid} not found (may already be terminated)")
+            except Exception as e:
+                print(f"Failed to kill PID {pid}: {e}")
+
+    pids = get_pids_using_port(PORT)
+    if pids:
+        kill_pids(pids)
 
 
 def main():
