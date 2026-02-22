2cbd219092d1ff6745df445b5d739032d62cfa7a
Arslan8 <arslankhan52@gmail.com>
2026-02-22 02:19:39 -0500

    removing my local changes, adding missing file

---
+++ virtuals/introspection/inspct.c
@@ -0,0 +1,4 @@
+//TODO: Hammad this is where i need the argument, this file needs to be provided by the frontend (python script), it is an auto-generated file
+void inspct_init(int argc, char ** argv) {
+		load_fastdyn_schemas("/home/faculty/abk6349/data/fastdyn/fastdyn_work/schema.txt");
+}
