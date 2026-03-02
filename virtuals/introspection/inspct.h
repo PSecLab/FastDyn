57e1dddfd8f0f10257cd95e9e031df0ac4c98106
michaelprooney <rooney13@purdue.edu>
2026-03-02 18:05:02 +0000

    Almost done with physics recursive build, facing linking problems at runtime

--- virtuals/introspection/inspct.h
+++ virtuals/introspection/inspct.h
@@ -1,5 +1,15 @@
+
+
+#ifndef INSPECT_H
+#define INSPECT_H
+
 #include <virtuals.h>
+
 bool inspct_get_field(const char* struct_name, uint32_t base_addr, const char* field_name, void* out_buffer);
 uint32_t inspct_get_field_offset(const char* struct_name, const char* field_name);
 uint32_t inspct_get_symbol(const char* symbol_name);
 bool load_fastdyn_schemas(const char* filepath);
+
+int inspct_init(int argc, char ** argv, const char *schema_path);
+
+#endif // INSPECT_H
