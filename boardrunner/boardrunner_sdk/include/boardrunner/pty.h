2e973981f974e45a3f76483aed39d3a0cc39a9b1
MuhammadHammad001 <hammad.bashir543@gmail.com>
2026-06-28 14:13:03 -0400

    reached spi4 imu slave detection

--- boardrunner/boardrunner_sdk/include/boardrunner/pty.h
+++ boardrunner/boardrunner_sdk/include/boardrunner/pty.h
@@ -3,5 +3,5 @@
 #include <device.h>
 
 int api_pty_fd_gen(const char* dev_name);
-void api_pty_write_req(int fd, uint8_t value);
+int api_pty_write_req(int fd, uint8_t value);
 int api_pty_read_nonblock(int fd, uint8_t *buff);
